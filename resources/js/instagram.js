a8b9ba2c02d8a7431a87b3f6903f63bb904453a2
hetmisc <hetmisc@gmail.com>
2016-09-19 09:45:45 -0400

    Revert "Revert "add the profile user dropdown feature.""
    This reverts commit 0e85ba38f5a335e70ac38b7c44add650b779f8e9.

--- resources/js/instagram.js
+++ resources/js/instagram.js
@@ -11,3 +11,96 @@ app.config(function($routeProvider) {
         templateUrl: '/templates/feed.html'
     });
 });
+
+
+/*
+var loadUserProfile = function (userName) {
+
+    $('div#userName').text(userName);
+
+    var profile;
+
+    $.ajax('/profile/' + userName, {
+        async: true,
+        cache: false,
+        dataType: 'json',
+        method: 'GET',
+        success: function (data) {
+            loadProfileData(data);
+            loadUserTweets(userName);
+        },
+        error: function (jqXhr, status, error) {
+            alert('Unable to load profile: ' + error);
+        }
+    });
+};
+
+var loadProfileData = function (profile) {
+
+    if (profile) {
+        var user = profile.user;
+
+        if (user.userName === $('#currentUser').val()) {
+            // load for update
+            $('input#firstName').val(user.firstName);
+            $('input#middleName').val(user.middleName);
+            $('input#lastName').val(user.lastName);
+            $('input#firstName').show();
+            $('input#middleName').show();
+            $('input#lastName').show();
+            $('div#firstName').hide();
+            $('div#middleName').hide();
+            $('div#lastName').hide();
+            $('#updateProfileButton').show();
+        }
+        else {
+            // load for display
+            $('div#firstName').text(user.firstName);
+            $('div#middleName').text(user.middleName);
+            $('div#lastName').text(user.lastName);
+            $('input#firstName').hide();
+            $('input#middleName').hide();
+            $('input#lastName').hide();
+            $('div#firstName').show();
+            $('div#middleName').show();
+            $('div#lastName').show();
+            $('#updateProfileButton').hide();
+        }
+    }
+
+    $('#followers-div').text('');
+
+    if (profile.followers && profile.followers.length) {
+        for (var i = 0; i < profile.followers.length; i++) {
+            $('#followers-div').append("<div>" + profile.followers[i].userName + "</div>");
+        }
+    }
+    else {
+        $('#followers-div').html("<strong>You don't have any followers, but <em>I</em> love you!</strong>");
+    }
+
+    $('#following-div').follower({
+        following: profile.following,
+        userName: profile.user.userName
+    });
+};
+
+var loadHtml = function (url, cb) {
+
+    $.ajax(url, {
+        cache: false,
+        method: 'GET',
+        dataType: 'html',
+        error: function (jqXhr, status, err) {
+            alert('Unable to load url ' + url + ': ' + err);
+        },
+        success: function (html) {
+            $('#appContent').html(html);
+
+            if (cb) {
+                cb();
+            }
+        }
+    });
+};
+*/
